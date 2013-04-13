9b4c9019ab0e20173ee64cedb647c9edae886b68
csdnlzh <balance.lzh@gmail.com>
2013-04-13 12:08:50 +0800

    solution for prob 1

--- 1/1/main.cpp
+++ 1/1/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
-
+#include <stdlib.h>
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    int sum=0;
+    for(int i=1;i<1000;i++)
+    {
+        if (i%3==0 || i%5==0)
+        {
+            sum+=i;
+        }
+    }
+    cout<<"sum: "<<sum<<endl;
+
+    system("pause");
 }
